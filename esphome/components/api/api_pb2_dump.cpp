9a84fd5172269c5aa01d9dd1825634af341b1054
Tim Lunn <tl@smlight.tech>
2026-06-15 15:56:57 +1000

    Update api bindings

--- esphome/components/api/api_pb2_dump.cpp
+++ esphome/components/api/api_pb2_dump.cpp
@@ -435,8 +435,7 @@ template<> const char *proto_enum_to_string<enums::WaterHeaterMode>(enums::Water
   }
 }
 #endif
-template<>
-const char *proto_enum_to_string<enums::WaterHeaterCommandHasField>(enums::WaterHeaterCommandHasField value) {
+template<> const char *proto_enum_to_string<enums::WaterHeaterCommandHasField>(enums::WaterHeaterCommandHasField value) {
   switch (value) {
     case enums::WATER_HEATER_COMMAND_HAS_NONE:
       return ESPHOME_PSTR("WATER_HEATER_COMMAND_HAS_NONE");
@@ -575,8 +574,7 @@ template<> const char *proto_enum_to_string<enums::MediaPlayerFormatPurpose>(enu
 }
 #endif
 #ifdef USE_BLUETOOTH_PROXY
-template<>
-const char *proto_enum_to_string<enums::BluetoothDeviceRequestType>(enums::BluetoothDeviceRequestType value) {
+template<> const char *proto_enum_to_string<enums::BluetoothDeviceRequestType>(enums::BluetoothDeviceRequestType value) {
   switch (value) {
     case enums::BLUETOOTH_DEVICE_REQUEST_TYPE_CONNECT:
       return ESPHOME_PSTR("BLUETOOTH_DEVICE_REQUEST_TYPE_CONNECT");
@@ -625,8 +623,7 @@ template<> const char *proto_enum_to_string<enums::BluetoothScannerMode>(enums::
   }
 }
 #endif
-template<>
-const char *proto_enum_to_string<enums::VoiceAssistantSubscribeFlag>(enums::VoiceAssistantSubscribeFlag value) {
+template<> const char *proto_enum_to_string<enums::VoiceAssistantSubscribeFlag>(enums::VoiceAssistantSubscribeFlag value) {
   switch (value) {
     case enums::VOICE_ASSISTANT_SUBSCRIBE_NONE:
       return ESPHOME_PSTR("VOICE_ASSISTANT_SUBSCRIBE_NONE");
@@ -729,8 +726,7 @@ template<> const char *proto_enum_to_string<enums::AlarmControlPanelState>(enums
       return ESPHOME_PSTR("UNKNOWN");
   }
 }
-template<>
-const char *proto_enum_to_string<enums::AlarmControlPanelStateCommand>(enums::AlarmControlPanelStateCommand value) {
+template<> const char *proto_enum_to_string<enums::AlarmControlPanelStateCommand>(enums::AlarmControlPanelStateCommand value) {
   switch (value) {
     case enums::ALARM_CONTROL_PANEL_DISARM:
       return ESPHOME_PSTR("ALARM_CONTROL_PANEL_DISARM");
@@ -847,6 +843,7 @@ template<> const char *proto_enum_to_string<enums::SerialProxyStatus>(enums::Ser
   }
 }
 #endif
+
 
 const char *HelloRequest::dump_to(DumpBuffer &out) const {
   MessageDumpHelper helper(out, ESPHOME_PSTR("HelloRequest"));
